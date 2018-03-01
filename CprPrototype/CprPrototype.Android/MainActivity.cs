f8724475b1b492b8beadfaf6f4fe932f3676696a
mortenmathiasen <mmat@tranebaerkaeret.dk>
2018-03-01 11:37:57 +0100

    Submit crash reports

--- CprPrototype/CprPrototype.Android/MainActivity.cs
+++ CprPrototype/CprPrototype.Android/MainActivity.cs
@@ -2,12 +2,18 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Graphics;
+using HockeyApp.Android;
 
 namespace CprPrototype.Droid
 {
     [Activity(Label = "CprPrototype", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            CrashManager.Register(this, "370caf726d08406d9ec1476ff2bbc9cf");
+        }
 
         protected override void OnCreate(Bundle bundle)
         {
