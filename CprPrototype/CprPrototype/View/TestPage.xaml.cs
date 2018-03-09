a4a484be093bed64dd02522e6513e271735cb868
Khalid Jamal <kjs@EAA-SH-KJS>
2018-03-09 15:34:32 +0100

    - Extra drugs still under construction

---
+++ CprPrototype/CprPrototype/View/TestPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CprPrototype.ViewModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CprPrototype.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TestPage : ContentPage
+	{
+        public static BaseViewModel _bvm = BaseViewModel.Instance;
+		public TestPage ()
+		{
+            InitializeComponent();
+
+           
+            DataTemplate template = new DataTemplate(typeof(DrugCell));
+            template.SetBinding(DrugCell.LabelTextProperty, "Tlabel");
+            lstDrugs.ItemTemplate = template;
+            lstDrugs.ItemsSource = _bvm.NotificationQueue;
+		}
+	}
+}
