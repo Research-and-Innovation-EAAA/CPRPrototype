71822e74c88ff7f01e2520721bb87f71d7ea3368
Khalid Jamal <kjs@EAA-SH-KJS>
2018-03-13 15:46:03 +0100

    - New extra drug feature still under construction

---
+++ CprPrototype/CprPrototype/View/ExtraDrugsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CprPrototype.View
+{
+    public class ExDrugsModel
+    {
+        public List<ExDrugs> Exdrugs { get; set; }
+        public ExDrugsModel()
+        {
+            Exdrugs = new ExDrugs().GetExtraDrugs();
+        }
+    }
+}
